ae0cdddc9c62b2924267f5027e81aea9ec6470ff
Evgeny <zobnin@gmail.com>
2021-07-29 18:01:05 +0300

    add buttons to dialog

--- dialog_sample.lua
+++ dialog_sample.lua
@@ -1,7 +1,15 @@
 function onResume()
-    ui:showText("Click to open dialog")
+    ui:showLines({ "Click to open dialog", "Click to open dialog with custom buttons" })
 end
 
-function onClick()
-    ui:showDialog("Dialog title", "This is dialog")
+function onClick(idx)
+    if idx == 1 then
+        ui:showDialog("Dialog title", "This is dialog")
+    elseif idx == 2 then
+        ui:showDialog("Dialog title", "This is dialog", "Button 1", "Button 2")
+    elseif idx == 100 then
+        ui:showToast("Button 1 clicked!")
+    elseif idx == 200 then
+        ui:showToast("Button 2 clicked!")
+    end
 end
