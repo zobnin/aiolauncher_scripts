8dfc1f632435433f2f5f9f36c8d84b90a793c4f3
Evgeny <zobnin@gmail.com>
2021-07-29 18:21:52 +0300

    bug fixes

--- dialog_sample.lua
+++ dialog_sample.lua
@@ -7,9 +7,13 @@ function onClick(idx)
         ui:showDialog("Dialog title", "This is dialog")
     elseif idx == 2 then
         ui:showDialog("Dialog title", "This is dialog", "Button 1", "Button 2")
-    elseif idx == 100 then
+    end
+end
+
+function onDialogClick(idx)
+    if idx == 1 then
         ui:showToast("Button 1 clicked!")
-    elseif idx == 200 then
+    elseif idx == 2 then
         ui:showToast("Button 2 clicked!")
     end
 end
