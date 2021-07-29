bb0bdd36a62f0944bce619ca74a61f1de8448fe4
Evgeny <zobnin@gmail.com>
2021-07-29 17:16:48 +0300

    add isWidgetAdded and showDialog functions

---
+++ dialog_sample.lua
@@ -0,0 +1,7 @@
+function onResume()
+    ui:showText("Click to open dialog")
+end
+
+function onClick()
+    ui:showDialog("Dialog title", "This is dialog")
+end
---
+++ is_widget_added_sample.lua
@@ -0,0 +1,3 @@
+function onResume()
+    ui:showText("Mail widget on the screen: " .. tostring(aio:isWidgetAdded("mail")))
+end
