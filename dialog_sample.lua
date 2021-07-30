46407b5e78dd270aae1d4eeff6721b748947a159
Evgeny <zobnin@gmail.com>
2021-07-30 18:51:37 +0300

    add ui:editDialog

--- dialog_sample.lua
+++ dialog_sample.lua
@@ -1,5 +1,9 @@
 function onResume()
-    ui:showLines({ "Click to open dialog", "Click to open dialog with custom buttons" })
+    ui:showLines({ 
+        "Click to open dialog", 
+        "Click to open dialog with custom buttons",
+        "Click to open edit dialog",
+    })
 end
 
 function onClick(idx)
@@ -7,13 +11,17 @@ function onClick(idx)
         ui:showDialog("Dialog title", "This is dialog")
     elseif idx == 2 then
         ui:showDialog("Dialog title", "This is dialog", "Button 1", "Button 2")
+    elseif idx == 3 then
+        ui:showEditDialog("Dialog title", "Write any text", "default text")
     end
 end
 
-function onDialogClick(idx)
-    if idx == 1 then
+function onDialogAction(value)
+    if value == 1 then
         ui:showToast("Button 1 clicked!")
-    elseif idx == 2 then
+    elseif value == 2 then
         ui:showToast("Button 2 clicked!")
+    elseif type(value) == "string" then
+        ui:showToast("Text: "..value)
     end
 end
---
+++ shell-widget.lua
@@ -0,0 +1,29 @@
+-- name = "Shell widget"
+-- description = "Shows the result of executing console commands"
+-- type = "widget"
+-- author = "Evgeny Zobnin (zobnin@gmail.com)"
+-- version = "1.0"
+
+currentOutput = "empty"
+
+function onResume()
+    redraw()
+end
+
+function redraw()
+    ui:showText(currentOutput)
+end
+
+function onClick(idx)
+    ui:showEditDialog("Enter command")
+end
+
+function onDialogAction(text)
+    system:exec(text)
+end
+
+function onShellResult(text)
+    currentOutput = text
+    redraw()
+end
+
