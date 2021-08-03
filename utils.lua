8a74f34102f054046a2d4f3065f5f2443e7cbeee
Evgeny <zobnin@gmail.com>
2021-08-03 13:36:53 +0300

    add ui:set_folding_flag

--- utils.lua
+++ utils.lua
@@ -11,3 +11,10 @@ function get_args_kv()
 
     return { keys, values }
 end
+
+function round(x, n)
+    local n = math.pow(10, n or 0)
+    local x = x * n
+    if x >= 0 then x = math.floor(x + 0.5) else x = math.ceil(x - 0.5) end
+    return x / n
+end
