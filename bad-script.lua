ca121af91bcc70fa3cb434349d8334c3cede2b1e
Evgeny <zobnin@gmail.com>
2021-08-03 21:50:32 +0300

    change set_title behaviour

---
+++ bad-script.lua
@@ -0,0 +1,2 @@
+function on_resume()
+end
--- title-sample.lua
+++ title-sample.lua
@@ -1,4 +1,4 @@
 function on_resume()
-    ui:show_text("Original title: "..ui:get_default_title())
     ui:set_title("New title")
+    ui:show_text("Original title: "..ui:get_default_title())
 end
