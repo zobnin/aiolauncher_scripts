bfa224ea1bd5ec58cd8e7748d1e9319ff35e7066
Evgeny <zobnin@gmail.com>
2021-08-09 17:38:57 +0300

    update

--- samples/chart-sample.lua
+++ samples/chart-sample.lua
@@ -1,8 +1,8 @@
 function on_resume()
     local points = { 
-        { 1628501740654, 123 },
-        { 1628503740654, 300 },
-        { 1628505740654, 175 },
+        { 1628501740654, 123456789 },
+        { 1628503740654, 300000000 },
+        { 1628505740654, 987654321 },
     }
-    ui:show_chart("Test", points, "MM.dd")
+    ui:show_chart("Test", points, "x:date y:number")
 end
