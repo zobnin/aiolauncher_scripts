8364138f5502addcc45e62c8c6b2e3a1d5e34bf2
Evgeny <zobnin@gmail.com>
2021-08-10 12:25:17 +0300

    add ui:prepare_context_menu

---
+++ samples/menu-sample.lua
@@ -0,0 +1,15 @@
+function on_resume()
+    ui:show_lines({ 
+        "1 item", "2 item", "3 item" 
+    })
+    
+    ui:prepare_context_menu({
+        { "share", "Menu item 1" },
+        { "copy",  "Menu item 2" },
+        { "trash", "Menu item 3" },
+    })
+end
+
+function on_context_menu_click(item_idx, menu_idx)
+    ui:show_toast("Clicked "..menu_idx.." menu item on "..item_idx.." screen element")
+end
