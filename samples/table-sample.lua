ddbf4a72e8f2bd868a44490a60f161d18cf008ee
ezobnin <zobnin@gmail.com>
2021-08-17 20:44:21 +0300

    fixes

--- samples/table-sample.lua
+++ samples/table-sample.lua
@@ -1,9 +1,14 @@
 function on_resume()
     local table = {
-        {"1", "2", "3"},
-        {"4", "5", "6"},
-        {"7", "8", "9"},
+        "1", "20", "30",
+        "40", "5", "66",
+        "07", "28", "9",
     }
 
-    ui:show_table(table, true)
+    ui:show_table(table, 3, true)
 end
+
+function on_click(idx)
+    ui:show_toast("Cell: "..idx)
+end
+
