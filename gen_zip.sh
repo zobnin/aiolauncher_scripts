96b0d033283074b451b11285efd5e483b40b88a5
Evgeny <zobnin@gmail.com>
2021-08-19 18:27:01 +0300

    reorganization

--- gen_zip.sh
+++ gen_zip.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 rm -rf scripts.zip scripts.md5
-zip -r scripts.zip -j main/*.lua community/*.lua ru/*.lua
+zip -r scripts.zip -j main/*.lua ru/*.lua
 md5sum scripts.zip | cut -d " " -f 1 > scripts.md5
