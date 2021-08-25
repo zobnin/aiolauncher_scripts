4a68d00e65461083b37a2a40dcec19e8349583ce
Evgeny <zobnin@gmail.com>
2021-08-25 09:52:39 +0300

    remove show_grid function

--- samples/table-sample2.lua
+++ samples/table-sample2.lua
@@ -1,10 +1,9 @@
 function on_resume()
     local tab = {
-        { "1234567890" },
         { "1", "2", "3" },
         { "4", "5", "6" },
         { "7", "8", "9" },
     }
 
-    ui:show_table(tab)
+    ui:show_table(tab, 0, true)
 end
