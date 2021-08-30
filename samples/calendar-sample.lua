c59901b2dc32e514c50c6bea0a84f1d1fe76a50c
Evgeny <zobnin@gmail.com>
2021-08-30 14:22:42 +0300

    add calendar api

---
+++ samples/calendar-sample.lua
@@ -0,0 +1,17 @@
+events = {}
+
+function on_resume()
+    local ev_titles = {}
+
+    events = slice(calendar:get_events(), 1, 10)
+
+    for k,v in ipairs(events) do
+        ev_titles[k] = v.title
+    end
+
+    ui:show_lines(ev_titles)
+end
+
+function on_click(idx)
+    calendar:open_event(events[idx].id)
+end
