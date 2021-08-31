5ece22761afc26114b18dc8c333a420c2c373b8f
Evgeny <zobnin@gmail.com>
2021-08-31 11:09:15 +0300

    add battery widget (fix)

--- community/battery-widget.lua
+++ community/battery-widget.lua
@@ -1,6 +1,15 @@
 -- name = "Battery info"
 
-function on_resume()
+ticks = 0
+
+function on_tick()
+  -- Update one time per 10 seconds
+  if ticks % 10 ~= 0 then
+    return
+  end
+
+  ticks = 0
+
   local batt_info = system:get_battery_info()
   local batt_strings = stringify_table(batt_info)
   ui:show_lines(batt_strings)
