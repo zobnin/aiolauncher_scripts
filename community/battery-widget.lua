ea079e220f2cfa6ee3d0c209e80d711f4e63361b
Evgeny <zobnin@gmail.com>
2021-09-01 13:14:44 +0300

    cleanup

--- community/battery-widget.lua
+++ community/battery-widget.lua
@@ -3,29 +3,29 @@
 ticks = -1
 
 function on_tick()
-  -- Update one time per 10 seconds
-  ticks = ticks + 1
-  if ticks % 10 ~= 0 then
-    return
-  end
+    -- Update one time per 10 seconds
+    ticks = ticks + 1
+    if ticks % 10 ~= 0 then
+        return
+    end
 
-  ticks = 0
+    ticks = 0
 
-  local batt_info = system:get_battery_info()
-  local batt_strings = stringify_table(batt_info)
-  ui:show_lines(batt_strings)
+    local batt_info = system:get_battery_info()
+    local batt_strings = stringify_table(batt_info)
+    ui:show_lines(batt_strings)
 end
 
 function stringify_table(tab)
-  local new_tab = {}
+    local new_tab = {}
 
-  for k,v in pairs(tab) do
-    table.insert(new_tab, capitalize(k)..": "..tostring(v))
-  end
+    for k,v in pairs(tab) do
+        table.insert(new_tab, capitalize(k)..": "..tostring(v))
+    end
 
-  return new_tab
+    return new_tab
 end
 
 function capitalize(string)
-  return string:gsub("^%l", string.upper)
+    return string:gsub("^%l", string.upper)
 end
--- community/sys-info-widget.lua
+++ community/sys-info-widget.lua
@@ -3,30 +3,30 @@
 ticks = -1
 
 function on_tick()
-  -- Update one time per 10 seconds
-  ticks = ticks + 1
-  if ticks % 10 ~= 0 then
-    return
-  end
+    -- Update one time per 10 seconds
+    ticks = ticks + 1
+    if ticks % 10 ~= 0 then
+        return
+    end
 
-  ticks = 0
+    ticks = 0
 
-  local info = system:get_system_info()
-  local strings = stringify_table(info)
+    local info = system:get_system_info()
+    local strings = stringify_table(info)
 
-  ui:show_lines(strings)
+    ui:show_lines(strings)
 end
 
 function stringify_table(tab)
-  local new_tab = {}
+    local new_tab = {}
 
-  for k,v in pairs(tab) do
-    table.insert(new_tab, capitalize(k):replace("_", " ")..": "..tostring(v))
-  end
+    for k,v in pairs(tab) do
+        table.insert(new_tab, capitalize(k):replace("_", " ")..": "..tostring(v))
+    end
 
-  return new_tab
+    return new_tab
 end
 
 function capitalize(string)
-  return string:gsub("^%l", string.upper)
+    return string:gsub("^%l", string.upper)
 end
