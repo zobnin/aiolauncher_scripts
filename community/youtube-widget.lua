d1951602807ab8320ae4b2905ce2d1de9d2ac9f6
sriramsv <sriramsv1991@gmail.com>
2022-08-28 18:43:26 -0700

    Youtube in app search search widget

---
+++ community/youtube-widget.lua
@@ -0,0 +1,33 @@
+-- name = "Youtube"
+-- description = "Search anything in youtube app"
+-- data_source = "internal"
+-- type = "search"
+-- author = "Sriram SV"
+-- version = "1.0"
+-- prefix = "youtube"
+
+text_from = ""
+text_to = ""
+yt_intent_action = "android.intent.action.SEARCH"
+yt_intent_category = "android.intent.category.DEFAULT"
+yt_package_name = "com.google.android.youtube"
+
+local md_color = require "md_colors"
+local red = md_colors.light_red_800
+
+function on_search(input)
+    text_from = input
+    text_to = ""
+    search:show({input},{red})
+end
+
+function on_click(idx)
+    intent:start_activity{
+        action = yt_intent_action,
+        category = yt_intent_category,
+        package = yt_package_name,
+        data = "query:"..text_from
+    }
+end
+
+
