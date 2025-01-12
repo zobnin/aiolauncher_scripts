3b033d6086cb3f5af965134903769ea731c49c42
Evgeny <zobnin@gmail.com>
2025-01-12 09:06:43 +0800

    Update what to do script to new API endpoint

--- community/what-to-do-widget.lua
+++ community/what-to-do-widget.lua
@@ -1,6 +1,6 @@
 -- name = "What to do?"
 -- description = "Let's find you something to do"
--- data_source = "https://www.boredapi.com/"
+-- data_source = "https://bored.api.lewagon.com/"
 -- type = "widget"
 -- author = "Evgeny Zobnin (zobnin@gmail.com)"
 -- version = "1.0"
@@ -12,7 +12,7 @@ end
 
 function on_click()
     system:vibrate(100)
-    http:get("http://www.boredapi.com/api/activity/")
+    http:get("https://bored.api.lewagon.com/api/activity/")
 end
 
 function on_network_result(result)
