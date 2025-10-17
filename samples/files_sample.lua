de5864f79fca68fb8c6bccfc928052836cc32632
Evgeny <zobnin@gmail.com>
2025-10-17 11:24:36 +0300

    Fix files sample

--- samples/files_sample.lua
+++ samples/files_sample.lua
@@ -1,6 +1,6 @@
 function on_resume()
     files:delete("abc.txt")
-    files:write_text("abc.txt", "This is text")
-    local string = files:read_text("abc.txt")
+    files:write("abc.txt", "This is text")
+    local string = files:read("abc.txt")
     ui:show_text(string)
 end
